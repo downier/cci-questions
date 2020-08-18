cc5a45d933796a0a8739b0994d2b40bb992290b8
downier <raulishere@gmail.com>
2020-08-17 18:17:02 -0700

    Problem staging

---
+++ Arrays and Strings/URLify.js
@@ -0,0 +1,9 @@
+//Write a method to replace all spaces in a string with '%20' 
+//input "Mr John Smith", 13
+//output 'Mr%20John%20Smith'
+
+function URLify(string, integer){
+  let output = ''
+  
+  return output;
+}
