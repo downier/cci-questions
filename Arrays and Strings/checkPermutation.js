981decf188eb52f54d9b5485242c2a31079b6399
downier <raulishere@gmail.com>
2020-08-17 18:09:45 -0700

    Check permutation solution

--- Arrays and Strings/checkPermutation.js
+++ Arrays and Strings/checkPermutation.js
@@ -1,1 +1,15 @@
 // Given two strings, write a method to decide if one is a permutation of the other.
+function checkPermutation(x, y) {
+  if (x.length !== y.length)return false
+  for(i = 0; i < x.length; i++){
+    if(x.split('').sort()[i] !== y.split('').sort()[i]) return false
+  }
+  return true;
+};
+
+let string1 = 'hello';
+let string2 = 'loleh';
+let string3 = 'loleho';
+
+console.log(checkPermutation(string1, string2))
+console.log(checkPermutation(string1, string3))
