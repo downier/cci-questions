2c2e1472eb7723d5b6aa29c61b2e42555a38915a
downier <raulishere@gmail.com>
2020-08-19 15:11:21 -0700

    Palindrome Permutation solution

--- Arrays and Strings/palindromePermutation.js
+++ Arrays and Strings/palindromePermutation.js
@@ -1,1 +1,19 @@
 // Given a string, write a function to check if it is a permutation of a palindrome
+
+function palindromePermutaton(string){
+  let array = string.split('');
+  let count = {};
+  for(i = 0; i < array.length; i++){
+    array[i] in count ? count[array[i]]+=1 : count[array[i]] = 1;
+  }
+  let odd = 0;
+  for (const [key, value] of Object.entries(count)) {
+    if (value === 1)odd++
+  }
+  if (odd > 1)return false;
+  return true
+}
+
+let test = 'octo ca t'
+
+console.log(palindromePermutaton(test))
