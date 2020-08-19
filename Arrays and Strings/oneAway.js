9a70461969ac7dbd031cacb46c7d24740049c51e
downier <raulishere@gmail.com>
2020-08-19 15:15:27 -0700

    Staging

--- Arrays and Strings/oneAway.js
+++ Arrays and Strings/oneAway.js
@@ -4,3 +4,12 @@
 // EX
 // page, ple -> true
 // pale, bake -> false
+
+function oneAway(x,y){
+  if (x === y)return true;
+  if (Math.abs(x.length - y.length) > 1)return false;
+  return true;
+}
+let test1 = 'hello'
+let test2 = 'hell'
+console.log(oneAway(test1,test2))
