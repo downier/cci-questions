437f11bfdaecada383b37ad8e8a797dfe4b90f5c
downier <raulishere@gmail.com>
2020-08-21 15:45:53 -0700

    Doesnt check if possible to insert

--- Arrays and Strings/oneAway.js
+++ Arrays and Strings/oneAway.js
@@ -2,14 +2,21 @@
 // remove a character, or replace a character. Given two strings, write a function to check if they are
 // one edit(or zero edits) away.
 // EX
-// page, ple -> true
+// pale, ple -> true
 // pale, bake -> false
 
 function oneAway(x,y){
   if (x === y)return true;
   if (Math.abs(x.length - y.length) > 1)return false;
+  let count = 0;
+  for(i = 0; i < x.length; i++) {
+    if(x.split('')[i] !== y.split('')[i]){
+      count++;
+    }
+    if(count > 1)return false;
+  }
   return true;
 }
-let test1 = 'hello'
-let test2 = 'hell'
+let test1 = 'pale'
+let test2 = 'ple'
 console.log(oneAway(test1,test2))
