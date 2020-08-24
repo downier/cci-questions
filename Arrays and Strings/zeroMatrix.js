22e732ca34a11099a5238238b7c8a3025abd0694
downier <raulishere@gmail.com>
2020-08-24 16:04:17 -0700

    Storing work

--- Arrays and Strings/zeroMatrix.js
+++ Arrays and Strings/zeroMatrix.js
@@ -1,11 +1,34 @@
 //Write an algorithm such that if an element in MxN matrix is 0, its entire row and column are set to 0.
 function zeroMatrix(matrix){
   let newMatrix = [];
+  let locations = [];
   for(i = 0; i < matrix.length; i++){
     let row = []; 
-    for(j = 0; j <matrix.length; j++){
-      
+    let zeroCount = 0;
+    for(j = 0; j < matrix.length; j++){
+      row.push(matrix[i][j]);
+      if(matrix[i][j] == 0){
+        zeroCount++;
+        locations.push([i,j]);
+      }
+    }
+    if(zeroCount == 0){
+      newMatrix.push(row)
+    } else {
+      let zeros = [];
+      zeros.length = matrix.length;
+      newMatrix.push(zeros.fill(0))
     }
   }
-  return arr;
+
+
+  return newMatrix;
 }
+
+let test = [
+  [1,2,0],
+  [4,5,6],
+  [7,8,9],
+];
+
+console.log(zeroMatrix(test))
