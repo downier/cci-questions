bdd8613c17df8a8e8b6500051954143b42cee2d9
downier <raulishere@gmail.com>
2020-09-09 17:43:47 -0700

    Updated answer

--- Arrays-and-Strings/zeroMatrix.js
+++ Arrays-and-Strings/zeroMatrix.js
@@ -1,25 +1,57 @@
 //Write an algorithm such that if an element in MxN matrix is 0, its entire row and column are set to 0.
-function zeroMatrix(matrix){
-  let newMatrix = [];
-  let locations = [];
-  for(i = 0; i < matrix.length; i++){
-    let row = []; 
-    let zeroCount = 0;
-    for(j = 0; j < matrix.length; j++){
-      row.push(matrix[i][j]);
-      if(matrix[i][j] == 0){
-        zeroCount++;
-        locations.push([i,j]);
-      }
-    }
-    if(zeroCount == 0){
-      newMatrix.push(row)
-    } else {
-      let zeros = new Array(matrix.length).fill(0);
-      newMatrix.push(zeros.fill(0))
+function zeroMatrix(matrix) {
+  let firstColHasZero = false;
+  let firstRowHasZero = false;
+
+  // Check if first col has zero
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] === 0) {
+      firstColHasZero = true;
+      break;
     }
   }
-  return newMatrix;
+
+  // Check if first row has zero
+  for (let j = 0; j < matrix[0].length; j++) {
+    if (matrix[0][j] === 0) {
+      firstRowHasZero = true;
+      break;
+    }
+  }
+
+  // Use first row and col as flags, set matrix[i][0] and matrix[0][j] to 0 if matrix[i][j] is 0
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+
+  // Zero out cells based on flags in first row and col
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[0].length; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  // Zero out first col
+  if (firstColHasZero) {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+
+  // Zero out first row
+  if (firstRowHasZero) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+  return matrix;
 }
 
 let test = [
